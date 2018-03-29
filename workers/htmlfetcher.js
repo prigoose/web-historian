89a456a0552e3f806500677b51f23e66b3bc3c32
PRIGOOSE <PRIYALGHOSE@GMAIL.COM>
2018-03-29 11:13:06 -0700

    Create directory for site being archived

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,16 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+const request = require('request');
+var fs = require('fs');
+
+exports.archiveURL = function(url) {
+
+  fs.mkdirSync(`./archives/sites/${url}`);
+  
+  // request(url, (err, res, body) => {
+  //   if (err) { return console.log(err); }
+  //   console.log(body.url);
+  //   console.log(body.explanation);
+  // });
+};
